9dd3fb93092f26254a3d86e34bccc1ce10c0f8a5
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-26 00:25:25 +0100

    add-milestone

---
+++ script.js
@@ -0,0 +1,25 @@
+// Descrizione:
+// Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+// - Prendando come riferimento il layout di esempio presente nell • html,
+const containerPost = document.queryselector( '#container' );
+Comment Code I ImproveCode
+const newPost = generateSing1ePost(sing1ePost);
+containerPost. innerHTML newPost;
+stampiamo i post del nostro feed.
+// - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore a1 testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai qua1i abbiamo messo il I ike-
+let counterLike = [l;
+const hand1eC1ickLike — document. querySeIector( • .like-button• ) ;
+Comment Code I Improve Code
+hand1eC1ickLike.addEventListener(' click',
+// cambio colore del] 'icona e del testo 'mi piace•
+const buttonText = document.queryselector(" .like-text");
+const imglcone — document.querySeIector(" .fa-thumbs-up" ) ;
+if (hand1eC1ickLike) {
+imglcone. classList. add ( "like-button- -liked" ) ;
+
+
+// Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
