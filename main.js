fd56e6b3235f1f2f3a81e3b79a99ac6161741091
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-26 00:37:41 +0100

    add-contain-html

--- main.js
+++ main.js
@@ -59,8 +59,44 @@ const posts = [
 // Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 
 //prendere il contaimner sul html
-const everyPostContainer = document.getElementById('container')
+const postContainer = document.getElementById('container')
 //Usare foreach per ogni pubblicazione per creare ogni singola pubblicazione
+
+every.foreach(post => {
+    const postHtml =
+    `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.author.image || 'https://via.placeholder.com/300'}" alt="${post.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div> <!-- Visualizzare la data di creazione della pubblicazione -->
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div> <!-- Visualizzare il contenuto della pubblicazione -->
+        <div class="post__image">
+            <img src="${post.media}" alt=""> <!-- Visualizzare l'immagine della pubblicazione -->
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone <!-- Visualizzare il contatore dei 'Mi Piace' -->
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `;
+})
 
 
 // Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
